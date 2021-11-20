ceace96c727a7b0a6c1f8becea4b7798448fedd9
walbagg <lrnmrcl13@gmail.com>
2021-11-20 14:25:19 +0800

    2nd

--- src/main/java/com/csa/samplefullstack/Controller/EmployeeController.java
+++ src/main/java/com/csa/samplefullstack/Controller/EmployeeController.java
@@ -4,6 +4,7 @@ import com.csa.samplefullstack.Entity.Employee;
 import com.csa.samplefullstack.Repository.EmployeeRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -16,7 +17,7 @@ public class EmployeeController {
     @Autowired
     private EmployeeRepository employeeRepository;
 
-    @RequestMapping("/employees")
+    @GetMapping("/employees")
     public List<Employee> getAllEmployees(){
         return employeeRepository.findAll();
     }
